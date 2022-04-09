75d29e5d5f2ecf7b53ce633290e36335d2f48e3c
zolfaShefreie <z.shefreie@gmail.com>
2022-04-09 22:38:36 +0430

    add arguments

--- spark/wordcount.py
+++ spark/wordcount.py
@@ -1,7 +1,7 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 import time
-
+import argparse
 
 file_paths = ['hdfs:/user/ebrahimi/file1G.txt', 'hdfs:/user/ebrahimi/file5G.txt', 'hdfs:/user/ebrahimi/file10G.txt']
 
@@ -13,15 +13,28 @@ def delete_punctuation(x):
     return new_str.strip()
 
 
+def validate_args(args):
+    if not (args.hdfs_input and args.hdfs_output):
+        raise Exception("enter path of files")
+    else:
+        return f"hdfs:{args.hdfs_input}", f"hdfs:{args.hdfs_output}"
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hdfs_input", help="path of input hdfs file")
+    parser.add_argument("--hdfs_output", help="path of output hdfs file")
+    input_path, output_path = validate_args(parser.parse_args())
+
     # conf = SparkConf().setAppName("app_sh")
     # sparkContent = SparkContext(conf=conf)
     spark = SparkSession.builder.appName("wordcount_sh").getOrCreate()
     sparkContent = spark.sparkContext
-    text_rdd = sparkContent.textFile(file_paths[0])
+    
+    text_rdd = sparkContent.textFile(input_path)
     start_time = time.time()
     print("start map-reduceing")
     words = text_rdd.flatMap(lambda line: line.split(" ")).filter(lambda x: x.strip())
     words = words.map(delete_punctuation).map(lambda x: x.lower())
-    result = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b)
-    print(f"done in: {time.time() - start_time}(s) ")
+    words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b).saveAsTextFile(output_path)
+    print(f"done in: {time.time() - start_time}(seconds) ")
