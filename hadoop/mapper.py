50ee1b67b98f4322c97b7a524f0a3417cc7b1fd5
zolfaShefreie <z.shefreie@gmail.com>
2022-04-09 12:06:37 +0430

    add mapper

---
+++ hadoop/mapper.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+from stopword_list import stopwords
+
+
+class TextCleaner:
+    punc = '''!()-[]{.};:'"\,<>/?@#$%^&*_~`|’“”…—–'''
+    removed_plus = stopwords
+
+    @classmethod
+    def run(cls, text: str) -> list:
+        """
+            run textCleaner for input string
+        Args:
+            text (str): input string
+        Returns:
+            list: token list
+        """
+        return cls.tokenize(cls.normlize(text))
+
+    @classmethod
+    def tokenize(cls, text: str) -> list:
+        """
+            tokenize, delete eng stopwords
+        Args:
+            text (str): input string
+        Returns:
+            list: tokens
+        """
+        stop_words = cls.removed_plus
+        tokens = text.split()
+        return [token.strip() for token in tokens if token.strip() and token.strip() not in stop_words]
+
+    @classmethod
+    def normlize(cls, text: str) -> str:
+        """
+            lower, delete numbers, delete punc, strip
+        Args:
+            text (str): input string
+        Returns:
+            str: normlized string
+        """
+        text = text.lower()
+        text = re.sub(r"\d+", '', text)
+
+        for each in cls.punc:
+            text = text.replace(each, " ")
+
+        text.strip()
+
+        return text
+
+
+if __name__ == "__main__":
+    """
+    mapper
+    """
+
+    text_cleaner = TextCleaner()
+
+    for line in sys.stdin:
+        words = text_cleaner.run(line)
+        for word in words:
+            print('%s\t%s' % (word, 1))
