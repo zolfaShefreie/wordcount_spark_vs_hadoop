1f58d24e502e9cee0ea88f69f93c26d69c50965a
zolfaShefreie <z.shefreie@gmail.com>
2022-04-10 14:36:53 +0430

    Update wordcount.py

--- spark/wordcount.py
+++ spark/wordcount.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     words = text_rdd.flatMap(lambda line: line.split(" ")).filter(lambda x: x.strip())
     words = words.map(delete_punctuation).map(lambda x: x.lower())
     result = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a+b).sortBy(lambda a: a[1], ascending=False)
-    print(f"map-reduce actions done in: {time.time() - start_time}(seconds) ")
     result.saveAsTextFile(output_path)
+    print(f"map-reduce actions done in: {time.time() - start_time}(seconds) ")
